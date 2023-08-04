760f0a34d301101f8ddfda06fc3fa135005f641d
we2beast <89505105005@mail.ru>
2023-08-04 20:44:32 +0200

    Add basic auth

--- src/main/java/com/poluhin/ss/demo/config/WebSecurityConfig.java
+++ src/main/java/com/poluhin/ss/demo/config/WebSecurityConfig.java
@@ -2,19 +2,23 @@ package com.poluhin.ss.demo.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain basicApiFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain basicApiFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
 }
